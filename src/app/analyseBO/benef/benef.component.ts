83de3740fb45e752b433d1aae7b7661147a625bb
Finaritra <finaritra.mickaelle@gmail.com>
2022-04-04 22:17:12 +0300

    [Affichage] Create components benef et ca

---
+++ src/app/analyseBO/benef/benef.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-benef',
+  templateUrl: './benef.component.html',
+  styleUrls: ['./benef.component.css']
+})
+export class BenefComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
