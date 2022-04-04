4239053f8c230fdb19610d5c26bb0864b0fbe5c7
Finaritra <finaritra.mickaelle@gmail.com>
2022-04-04 22:16:49 +0300

    [Affichage] Create components analyse

---
+++ src/app/analyse-bo/analyse-bo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-analyse-bo',
+  templateUrl: './analyse-bo.component.html',
+  styleUrls: ['./analyse-bo.component.css']
+})
+export class AnalyseBOComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
